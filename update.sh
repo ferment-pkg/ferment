4f99a335bd1507e0b2fabbf153a10e3ba1fdc6f8
NotTimIsReal <alastairrintumathew@gmail.com>
2022-05-15 19:41:40 +1000

    edited update.sh

--- update.sh
+++ update.sh
@@ -6,26 +6,28 @@ echo "Getting Latest Git Pull"
 result=$(git pull)
 function UpdatePackages(){
     echo "Now Updating Packages..."
-    r=$(cd Barrells)
-    if [ "$r" = "cd: no such file or directory: e"]
+    r=$(ls|grep Barrells)
+    if [ "$r" = "" ]
     then
         echo "Barrells Not Found"
         echo "Cloning Barrells"
         git clone https://github.com/ferment-pkg/Barrells Barrells
     else
+        r=$(cd Barrells)
         result=$(git pull)
-        if [ "$result" = "fatal: not a git repository (or any of the parent directories): .git" ]; then
+        if [ "$result" = "fatal: not a git repository (or any of the parent directories): .git" ] 
+        then
             echo "Packages Are Not Installed"
             echo "Installing Packages..."
             rm -rf $(ls -a)
             git clone https://github.com/ferment-pkg/Barrells .
+        fi
     fi
  
 }
 
 if [ "$result" = "Already up to date." ]; then
     echo "Already Up To Date"
-    echo "Updating Packages"
     UpdatePackages
     exit 0
 fi
